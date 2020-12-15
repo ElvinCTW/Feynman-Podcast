5cd58cac09dd350168325c7a93f2a3ef8362324a
ElvinCTW <ad81328@gmail.com>
2020-12-15 22:07:11 +0800

    include firebase app engine in service client

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module feynman-podcast
 go 1.14
 
 require (
+	firebase.google.com/go/v4 v4.1.0
 	code.sajari.com/docconv v1.1.0
 	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
 	github.com/advancedlogic/GoOse v0.0.0-20191112112754-e742535969c1 // indirect
@@ -13,5 +14,6 @@ require (
 	github.com/unidoc/unipdf/v3 v3.9.0
 	go.mongodb.org/mongo-driver v1.3.5
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	google.golang.org/api v0.17.0
 	gopkg.in/yaml.v2 v2.3.0
 )
--- internal/pkg/service/client.go
+++ internal/pkg/service/client.go
@@ -1,11 +1,15 @@
 package service
 
 import (
+	"context"
 	"feynman-podcast/internal/pkg/config"
 	"feynman-podcast/internal/pkg/crawler"
 	"feynman-podcast/internal/pkg/model"
 	"feynman-podcast/internal/pkg/upload"
 	"net/http"
+	firebase "firebase.google.com/go/v4"
+	"fmt"
+	"google.golang.org/api/option"
 	"sync"
 )
 
@@ -17,10 +21,19 @@ var (
 func NewClient(c *config.Config) *Client {
 	once.Do(func() {
 		httpClient := &http.Client{}
+		opt := option.WithCredentialsFile("test-helper-key.json")
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Println("unable to init app engine")
+		}
+
+		fmt.Println(app)
+
 		client = &Client{
 			ModelClient:   model.NewClient(c.Mongo.Database, c.Mongo.URI),
 			UploadClient:  upload.NewClient(c.Upload.AwsAccessKey, c.Upload.AwsAccessSecret, c.Upload.AwsRegion, c.Upload.AwsBucket),
 			CrawlerClient: crawler.NewClient(httpClient),
+			App:          app,
 		}
 	})
 
@@ -31,4 +44,5 @@ type Client struct {
 	*model.ModelClient
 	UploadClient  *upload.Client
 	CrawlerClient *crawler.Client
+	App          *firebase.App
 }
